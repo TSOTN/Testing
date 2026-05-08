74a6c5e6ea69644318e759eab5815d05b4b1dd47
TSOTN <leo2100821@gmail.com>
2026-05-09 00:08:02 +0200

    imagenes posts

--- frontend/explorar/script.js
+++ frontend/explorar/script.js
@@ -3,11 +3,11 @@
 
 const exploreData = [
   { type: 'juego', title: 'Hollow Knight', img: 'https://steamcdn-a.akamaihd.net/steam/apps/367520/header.jpg', desc: 'Metroidvania sombrío y preciso.' },
-  { type: 'pelicula', title: 'Akira', img: 'https://img.youtube.com/vi/0sK3D3t2JTE/maxresdefault.jpg', desc: 'Clásico anime cyberpunk.' },
+  { type: 'pelicula', title: 'Akira', img: 'https://m.media-amazon.com/images/M/MV5BM2M0MzYwZmQtYjE5ZC00YzMyLWJkYWYtZTAxNzQzN2ZjYjA3XkEyXkFqcGdeQXVyMTQxNzMzNTI@._V1_.jpg', desc: 'Clásico anime cyberpunk.' },
   { type: 'juego', title: 'Ori and the Blind Forest', img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/387290/header.jpg', desc: 'Plataformas y emoción.' },
-  { type: 'pelicula', title: 'Ghost in the Shell', img: 'https://img.youtube.com/vi/ztWT3rjVQ2M/maxresdefault.jpg', desc: 'Identidad y tecnología.' },
+  { type: 'pelicula', title: 'Ghost in the Shell', img: 'https://m.media-amazon.com/images/M/MV5BZjQ3ZjMwMTEtNDZjNy00NWJjLWI1MzAtZDIwYTQ0YzI1MDQxXkEyXkFqcGdeQXVyMTMxODk2OTk@._V1_.jpg', desc: 'Identidad y tecnología.' },
   { type: 'juego', title: 'Celeste', img: 'https://cdn.akamai.steamstatic.com/steam/apps/504230/header.jpg', desc: 'Plataformas desafiantes.' },
-  { type: 'pelicula', title: 'Paprika', img: 'https://img.youtube.com/vi/VIhC8T7A8qI/maxresdefault.jpg', desc: 'Sueños y realidad entrelazados.' }
+  { type: 'pelicula', title: 'Paprika', img: 'https://m.media-amazon.com/images/M/MV5BNjc5NDk3NjUtNWNhMy00MDA1LTljNTgtNDI0YTAyMjY3MzY1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg', desc: 'Sueños y realidad entrelazados.' }
 ];
 
 function initExplore() {
